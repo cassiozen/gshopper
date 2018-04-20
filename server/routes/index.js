6e226a059d763e039ef64f96cad68bb0a41d58d5
Cássio Zen <cassiozen@gmail.com>
2018-04-20 11:53:24 -0400

    Added api versioning

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
 module.exports = router
 
-router.use('/api', require('./api'))
+router.use('/api/v2', require('./api'))
 router.use('/auth', require('./auth'))
